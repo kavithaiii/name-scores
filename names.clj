1ccf9958d4cd0949e2b87e061c989792d281510b
kavithaiii <87979350+kavithaiii@users.noreply.github.com>
2021-07-27 10:44:34 +0530

    align the code

--- names.clj
+++ names.clj
@@ -1,12 +1,15 @@
 ;; This program uses names.txt 
 ;; execute this program at REPL prompt of clojure
 ;; copy this file and paste on REPL terminal
+
+(defn sum [x] (reduce + x))
+
 (defn find-char-value [name1]
   (def stringmap {"A" 1, "B" 2, "C" 3, "D" 4, "E" 5, "F" 6, "G" 7, "H" 8, "I" 9, "J" 10, "K" 11, "L" 12, "M" 13, "N" 14, "O" 15, "P" 16, "Q" 	17, "R" 18, "S" 19, "T" 20, "U" 21, "V" 22, "W" 23, "X" 24, "Y" 25, "Z" 26})
 ;; (def name1 "BETTY")
 ;; (println "name length: " (count name1))
 
-  (defn sum [x] (reduce + x))
+  ;; (defn sum [x] (reduce + x))
 
   (def sum-of-char (sum
                     (loop [i 0
@@ -32,7 +35,7 @@ sum-of-char)
       (def name-list-length (count sorted-names-list))
       (println "name list length " name-list-length)
 
-      (defn sum [x] (reduce + x))
+      ;; (defn sum [x] (reduce + x))
 
       (def sum-of-names (sum
                          (loop [i 0
@@ -40,13 +43,13 @@ sum-of-char)
                            (if (< i name-list-length)
                              (do
                                (def n1 (nth sorted-names-list i))
-                        ;; (println ">>>>>" n1 " is at postion " i)
+                             ;; (println ">>>>>" n1 " is at postion " i)
                                (def name-position-in-array (+ i 1))
-						;; (println "npInArray add one to it >>>" name-position-in-array)
+			     ;; (println "npInArray add one to it >>>" name-position-in-array)
                                (def name-value (* (find-char-value n1) name-position-in-array))
                                (println "sum of each char in a name MULTIPLIED by name position in array >>> " name-value)
                                (recur (inc i) (conj name-value-arr name-value)))
-                             name-value-arr)
+                             	name-value-arr)
                    		 )
                   		)
 	  )
